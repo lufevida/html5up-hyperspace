809d91cec374648abcc2ddd2da7399b8a9e5ac88
John Vandenberg <jayvdb@gmail.com>
2017-05-22 03:07:49 +0700

    Enable ESLint
    Fix a semicolon that should be a comma,
    resolving some undefined variable warnings.
    
    Avoid assignment in an if statement.
    
    Ignore html5shiv.js, as it is a minified file maintained elsewhere.

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,7 @@
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
+/* global skel */
 
 (function($) {
 
@@ -176,7 +177,8 @@
 						$image.css('background-image', 'url(' + $img.attr('src') + ')');
 
 					// Set background position.
-						if (x = $img.data('position'))
+						x = $img.data('position');
+						if (x)
 							$image.css('background-position', x);
 
 					// Hide <img>.
